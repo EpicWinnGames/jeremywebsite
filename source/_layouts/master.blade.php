8a8321938256b4889e00f04ee1c0a76f6b5ebdd2
Samantha Geitz <samanthamichele7@gmail.com>
2017-09-06 21:27:16 -0500

    Initial site

---
+++ source/_layouts/master.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <link href="https://fonts.googleapis.com/css?family=Josefin+Slab|Nunito" rel="stylesheet">
+        <link rel="stylesheet" href="/css/main.css">
+    </head>
+    <body>
+        @yield('body')
+    </body>
+</html>
